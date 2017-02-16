b4e533f5509706b622c4843c7c670091bf1c0b1c
Sean McCauliff <smccauliff@linkedin.com>
2017-02-16 14:58:27 -0800

    Distrbute jars to artifactory/jcenter.

---
+++ buildSrc/src/main/groovy/com/linkedin/gradle/build/DistributeTask.groovy
@@ -0,0 +1,40 @@
+package com.linkedin.gradle.build
+
+import groovy.json.JsonBuilder
+import org.apache.http.client.fluent.Request
+import org.apache.http.entity.ContentType
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
+
+class DistributeTask extends DefaultTask {
+
+    @TaskAction
+    public void distributeBuild() {
+        ArtifactoryPluginConvention convention = project.convention.plugins.artifactory
+        def buildNumber = convention.clientConfig.info.buildNumber
+        def buildName = convention.clientConfig.info.buildName
+        def context = convention.clientConfig.publisher.contextUrl
+        def password = convention.clientConfig.publisher.password
+
+	System.out.println("context " + context + " buildName " + buildName);
+        def body = [
+            "publish"              : "true",
+            "overrideExistingFiles": "false",
+            "async"                : "true",
+            "targetRepo"           : "maven",
+            "sourceRepos"          : [buildName],
+            "dryRun"               : "true"
+        ]
+
+        def bodyString = new JsonBuilder(body).toString()
+
+        def content = Request.Post("$context/api/build/distribute/$buildName/$buildNumber")
+            .bodyString(bodyString, ContentType.APPLICATION_JSON)
+            .addHeader("X-JFrog-Art-Api", password)
+            .execute()
+            .returnContent()
+
+        logger.lifecycle("Distribute Response: {}", content.asString())
+    }
+}
