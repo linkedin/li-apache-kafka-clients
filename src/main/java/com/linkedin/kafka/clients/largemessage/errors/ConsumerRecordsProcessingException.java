5e5bfe1b619aaaa9db58fc3732d4b43740a7555f
Jiangjie Qin <jqin@jqin-ld1.linkedin.biz>
2017-06-15 10:29:38 -0700

    Use suppressed exceptions.

--- src/main/java/com/linkedin/kafka/clients/largemessage/errors/ConsumerRecordsProcessingException.java
+++ src/main/java/com/linkedin/kafka/clients/largemessage/errors/ConsumerRecordsProcessingException.java
@@ -1,6 +1,7 @@
 package com.linkedin.kafka.clients.largemessage.errors;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -8,7 +9,13 @@ public class ConsumerRecordsProcessingException extends RuntimeException {
   private final List<RecordProcessingException> _recordProcessingExceptions;
 
   public ConsumerRecordsProcessingException(List<RecordProcessingException> exceptions) {
-    super(String.format("Received exception when processing messages for %d partitions.", exceptions.size()));
+    super(String.format("Received exception when processing messages for %d partitions.", exceptions.size()), exceptions.get(0));
+    Iterator<RecordProcessingException> exceptionIterator = exceptions.iterator();
+    // skip the first exception.
+    exceptionIterator.next();
+    while (exceptionIterator.hasNext()) {
+      addSuppressed(exceptionIterator.next());
+    }
     _recordProcessingExceptions = exceptions;
   }
 
