979b122a3fa2681eabb3693f3398533cafb13f27
Radai Rosenblatt <radai.rosenblatt@gmail.com>
2017-09-07 13:39:09 -0700

    require release tags to be _exactly_ of the form X.Y.Z (#62)

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -7,11 +7,11 @@ buildVersion=`grep -oP '^[\s]*version[\s]*=[\s]*\K([^\s]*)(?=([\s]*))' gradle.pr
 
 echo current tag is $currentTag, build version is $buildVersion
 
-if [ "x$currentTag" != "xRELEASE-$buildVersion" ]; then
+if [ "x$currentTag" != "x$buildVersion" ]; then
   echo "current tag version does not match project version"
   exit 1
 fi
 
 echo "Publishing a release"
-#./gradlew distributeBuild
+./gradlew distributeBuild
 
