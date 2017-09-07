dfd2571e7dabeee2e1d491f32bb5d66fdbf705e7
radai-rosenblatt <radai.rosenblatt@gmail.com>
2017-09-06 21:34:52 -0700

    take advantage of the new tag support in circleci 2.0 for releasing on tags

---
+++ .circleci/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+currentTag=`git describe --tags`
+buildVersion=`grep -oP '^[\s]*version[\s]*=[\s]*\K([^\s]*)(?=([\s]*))' gradle.properties`
+
+echo current tag is $currentTag, build version is $buildVersion
+
+if [ "x$currentTag" != "xRELEASE-$buildVersion" ]; then
+  echo "current tag version does not match project version"
+  exit 1
+fi
+
+echo "Publishing a release"
+#./gradlew distributeBuild
+
