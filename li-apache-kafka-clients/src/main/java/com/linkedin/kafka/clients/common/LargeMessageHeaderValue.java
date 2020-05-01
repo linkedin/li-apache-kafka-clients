fc7dc49c36ccab33559170963ce84bd90995830c
Navina Ramesh <nramesh@linkedin.com>
2020-05-01 11:24:11 -0700

    Adding original message size to the large message header

--- li-apache-kafka-clients/src/main/java/com/linkedin/kafka/clients/common/LargeMessageHeaderValue.java
+++ li-apache-kafka-clients/src/main/java/com/linkedin/kafka/clients/common/LargeMessageHeaderValue.java
@@ -10,10 +10,10 @@ import java.util.UUID;
 
 /**
  * This class represents the header value for a large message.
- * Every large message header takes up 25 bytes and is structured as follows
+ * Every large message header takes up 29 bytes and is structured as follows
  *
- * | Type   | UUID     | segmentNumber | numberOfSegments |
- * | 1 byte | 16 bytes | 4 bytes       | 4 bytes          |
+ * | Type   | UUID     | segmentNumber | numberOfSegments | messageSizeInBytes |
+ * | 1 byte | 16 bytes | 4 bytes       | 4 bytes          | 4 bytes            |
  *
  * The Large message header values will be used to support large messages eventually.
  * (as opposed to encoding large segment metadata info inside the payload)
@@ -21,21 +21,30 @@ import java.util.UUID;
 public class LargeMessageHeaderValue {
   public static final UUID EMPTY_UUID = new UUID(0L, 0L);
   public static final int INVALID_SEGMENT_ID = -1;
+  public static final int INVALID_MESSAGE_SIZE = -1;
   private final byte _type;
   private final UUID _uuid;
   private final int _segmentNumber;
   private final int _numberOfSegments;
+  private final int _messageSizeInBytes;
 
   // This indicates that the large message framework is using
   // SegmentSerializer/SegmentDeserializer interface to split
   // and assemble large message segments.
   public static final byte LEGACY = (byte) 0;
+  // This indicates that the segment metadata of a large message can be found in the record header and not in the payload
+  public static final byte LEGACY_V2 = (byte) 1;
 
-  public LargeMessageHeaderValue(byte type, UUID uuid, int segmentNumber, int numberOfSegments) {
+  public LargeMessageHeaderValue(byte type, UUID uuid, int segmentNumber, int numberOfSegments, int messageSizeInBytes) {
     _type = type;
     _uuid = uuid;
     _segmentNumber = segmentNumber;
     _numberOfSegments = numberOfSegments;
+    _messageSizeInBytes = messageSizeInBytes;
+  }
+
+  public int getMessageSizeInBytes() {
+    return _messageSizeInBytes;
   }
 
   public int getSegmentNumber() {
@@ -55,7 +64,8 @@ public class LargeMessageHeaderValue {
   }
 
   public static byte[] toBytes(LargeMessageHeaderValue largeMessageHeaderValue) {
-    byte[] serialized = new byte[25];
+    byte[] serialized = largeMessageHeaderValue.getType() == LEGACY ? new byte[25] : new byte[29];
+
     int byteOffset = 0;
     serialized[byteOffset] = largeMessageHeaderValue.getType();
     byteOffset += 1; // for type
@@ -66,6 +76,10 @@ public class LargeMessageHeaderValue {
     PrimitiveEncoderDecoder.encodeInt(largeMessageHeaderValue.getSegmentNumber(), serialized, byteOffset);
     byteOffset += PrimitiveEncoderDecoder.INT_SIZE; // for segment number
     PrimitiveEncoderDecoder.encodeInt(largeMessageHeaderValue.getNumberOfSegments(), serialized, byteOffset);
+    if (largeMessageHeaderValue.getType() == LEGACY_V2) { // We serialize the new field - messageSize
+      byteOffset += PrimitiveEncoderDecoder.INT_SIZE; // for message size
+      PrimitiveEncoderDecoder.encodeInt(largeMessageHeaderValue.getMessageSizeInBytes(), serialized, byteOffset);
+    }
     return serialized;
   }
 
@@ -81,6 +95,11 @@ public class LargeMessageHeaderValue {
     int segmentNumber = PrimitiveEncoderDecoder.decodeInt(bytes, byteOffset);
     byteOffset += PrimitiveEncoderDecoder.INT_SIZE;
     int numberOfSegments = PrimitiveEncoderDecoder.decodeInt(bytes, byteOffset);
-    return new LargeMessageHeaderValue(type, new UUID(mostSignificantBits, leastSignificantBits), segmentNumber, numberOfSegments);
+    if (bytes.length == 29) {
+      byteOffset += PrimitiveEncoderDecoder.INT_SIZE;
+      int messageSizeInBytes = PrimitiveEncoderDecoder.decodeInt(bytes, byteOffset);
+      return new LargeMessageHeaderValue(type, new UUID(mostSignificantBits, leastSignificantBits), segmentNumber, numberOfSegments, messageSizeInBytes);
+    }
+    return new LargeMessageHeaderValue(type, new UUID(mostSignificantBits, leastSignificantBits), segmentNumber, numberOfSegments, INVALID_MESSAGE_SIZE);
   }
 }
