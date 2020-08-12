9752c6e49e439597d55573b2c86d5b81497b68ac
Zhe Song <zhsong@zhsong-mn1.linkedin.biz>
2020-08-11 17:55:41 -0700

    Add counter metric for offset out of range & invalid exceptions in LiKafkaConsumerImpl

--- li-apache-kafka-clients/src/main/java/com/linkedin/kafka/clients/consumer/LiKafkaConsumerImpl.java
+++ li-apache-kafka-clients/src/main/java/com/linkedin/kafka/clients/consumer/LiKafkaConsumerImpl.java
@@ -719,10 +719,12 @@ public class LiKafkaConsumerImpl<K, V> implements LiKafkaConsumer<K, V> {
 
       if (!seekBeginningPartitions.isEmpty()) {
         LOG.info("Offsets are out of range for partitions {}. Seeking to the beginning offsets returned", seekBeginningPartitions);
+        _offsetInvalidOrOutRangeCount.getAndIncrement();
         seekBeginningPartitions.forEach(this::seekAndClear);
       }
       if (!seekEndPartitions.isEmpty()) {
         LOG.info("Offsets are out of range for partitions {}. Seeking to the end offsets returned", seekEndPartitions);
+        _offsetInvalidOrOutRangeCount.getAndIncrement();
         seekEndPartitions.forEach(this::seekAndClear);
       }
       if (!seekFetchedOffsetPartitions.isEmpty()) {
